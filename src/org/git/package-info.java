b2334d2a0910757b14aa7742a540b661cb88f853
vignesh158999 <selvavignesh333@gmail.com>
2022-05-02 14:45:27 +0530

    added

---
+++ src/org/git/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ELCOT
+ *
+ */
+package org.git;
---
+++ src/org/git/sample.java
@@ -0,0 +1,18 @@
+package org.git;
+
+public class sample {
+	private void test1() {
+		
+	}
+	private void test2() {
+		
+
+	}
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
